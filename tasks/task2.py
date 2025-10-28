50bf7341ee279fda8fd488dfb84bb8777610bdda
Safarov Arthur <ttttwobata@gmail.com>
2025-10-28 23:37:39 +0500

    Update task2.py

--- tasks/task2.py
+++ tasks/task2.py
@@ -2,7 +2,12 @@
 
 def solve():
 # Ниже пишите решение задачи
-
+    n = input()
+    a = int(n[0]) + int(n[1]) + int(n[2])
+    b = 0
+    for i in range(3, len(n)):
+    b += int(n[i])
+    print(a, b)
    
 
    
